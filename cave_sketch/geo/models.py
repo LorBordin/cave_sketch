0df515b47e2f0133a354c3363d503a46a9fef75c
lbordin <lorenzo.bordin@msg-global.com>
2026-05-13 15:31:54 +0200

    refactored georeferencing layer, created model, parsed code, added tests

---
+++ cave_sketch/geo/models.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class GpsRef:
+    """A known reference point linking a survey station ID to GPS coordinates."""
+    station_id: str
+    lat: float
+    lon: float
+
+@dataclass
+class GeoPoint:
+    """A survey point that has been georeferenced to GPS coordinates."""
+    station_id: str
+    lat: float
+    lon: float
+    x: float
+    y: float
